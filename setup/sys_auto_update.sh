dc6490283c749d38af77e9c881c76f7fe253a466
Giovanni Marinaccio <naccio@UNKNOWN-DEVICE>
2023-02-18 17:57:18 +0100

    autoremove autoconfirm

--- setup/sys_auto_update.sh
+++ setup/sys_auto_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 sudo apt-get update
+sudo apt-get install -y zip unzip tar git curl wget vim less
 sudo apt-get upgrade -y
-sudo apt-get install -y zip unzip tar git curl wget vi less
 sudo apt-get autoremove -y
